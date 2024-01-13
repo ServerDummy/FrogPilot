15a1138aeaab97047e51085e9d447f081cb04700
geniuth2 <141710362+geniuth2@users.noreply.github.com>
2024-01-13 18:09:11 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -333,7 +333,7 @@ class CarInterface(CarInterfaceBase):
         ret.longitudinalTuning.kpBP = [5., 35.]
         ret.longitudinalTuning.kpV = [0.1677, 0.1429]
         ret.longitudinalTuning.kiBP = [0., 35.0]
-        ret.longitudinalTuning.kiV = [0.0, 0.01]
+        ret.longitudinalTuning.kiV = [0.03, 0.01]
         ret.longitudinalTuning.kf = 0.15
         ret.stoppingDecelRate = 0.8
       else:  # Pedal used for SNG, ACC for longitudinal control otherwise
